5382cc374fa87dffd150e56ac2e92dcd75c20d7b
Bsfada <bsfada@gmail.com>
2013-04-28 22:50:14 +0300

    Revert "first"
    This reverts commit 275d1250e2651543cf524b8697729c0a8f0d7722.

--- header.php
+++
@@ -1,3 +0,0 @@
-<div class="header">
-	<a href="ganadores.php" <?=$page=="ganadores" ? 'class="selected"' : ''?> title="Ganadores">GANADORES</a><a href="index.php" class="logo"><img src="img/logo.png" alt="" /></a><a href="#" title="Empresas">EMPRESAS</a>
-</div>
