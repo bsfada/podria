cc84a64b1c774560b6adcc6640b23b11822da414
Bsfada <bsfada@gmail.com>
2013-04-28 22:54:11 +0300

    Revert "Revert "first""
    This reverts commit 5382cc374fa87dffd150e56ac2e92dcd75c20d7b.

---
+++ paso1.php
@@ -0,0 +1,27 @@
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Podria Ser Tuyo</title>
+
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+<script type="text/javascript" src="js/functions.js"></script>
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700,600&amp;subset=latin,latin-ext' rel='stylesheet' type='text/css' />
+<link href="css/style.css" rel="stylesheet" type="text/css" />
+
+</head>
+<body>
+<?php $page="paso1"; include('header.php') ?>
+
+<div class="content_wrapper">
+	<div class="gray_box_wrapper">
+	<div class="gray_box">
+		aaaaaaaaaa
+	</div>
+	</div>
+</div>	
+
+<?php include('footer.php') ?>
+
+</body>
+</html>
