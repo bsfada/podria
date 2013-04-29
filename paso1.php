db588e3ec9f360f2ab13607172ac62c3e5bda195
Bsfada <bsfada@gmail.com>
2013-04-29 22:22:14 +0300

    paso1

--- paso1.php
+++ paso1.php
@@ -16,7 +16,85 @@
 <div class="content_wrapper">
 	<div class="gray_box_wrapper">
 	<div class="gray_box">
-		aaaaaaaaaa
+		<div class="blue_box">
+			<table cellpadding="0" cellspacing="0" summary="list">
+				<tr>
+					<td class="header green">Pichiculi</td>
+					<td class="header orange">Medio y Medio</td>
+					<td class="header red">Banda Deluxo</td>
+				</tr>
+				<tr>
+					<td class="first green">
+						<div><span class="arrow">&nbsp;</span><strong>7 días</strong> de duración</div>
+
+						<div class="odd"><span class="arrow">&nbsp;</span><strong>Pag. Standard</strong> con info sobre el sorteo</div>
+
+						<div><span class="arrow">&nbsp;</span><strong>Diseño banner</strong> para HomePage e interna (la del propio sorteo)</div>
+
+						<div class="odd"><span class="arrow">&nbsp;</span><strong>Acceso a insights</strong> basicos de la campana (actualizados cada 30 mins)</div>
+
+						<div><span class="arrow">&nbsp;</span><strong>Posteo de anuncio</strong> sobre la campaña en FB y Twitter de PST</div>
+
+						<div class="odd"><span class="arrow">&nbsp;</span><strong>Mención</strong> en Newsletter</div>
+
+						<div><span class="arrow">&nbsp;</span><strong>Selección</strong>, notificación y entrega del premio al ganador</div>
+					</td>
+					<td class="orange">
+						<div><span class="arrow">&nbsp;</span><strong>15 días</strong> de duración</div>
+
+						<div class="odd"><span class="arrow">&nbsp;</span><strong>Customización Absoluta:</strong> Diseño banner para HomePage + interna (la del propio sorteo) + background personalizado</div>
+
+						<div><span class="arrow">&nbsp;</span><strong>Inclusion de un featured video</strong> en la pagina del concurso (opcional)</div>
+
+						<div class="odd"><span class="arrow">&nbsp;</span><strong>Acceso ilimitado a insights</strong> en tiempo real para monitoreo de campaña </div>
+
+						<div><span class="arrow">&nbsp;</span><strong>Reporte de performance</strong> pormenorizado al término de la campana</div>
+
+						<div class="odd"><span class="arrow">&nbsp;</span><strong>Almacenamiento</strong> de contactos para futura campaña</div>
+
+						<div><span class="arrow">&nbsp;</span><strong>Featured Post</strong> en la pag de FB de PST</div>
+
+						<div class="odd"><span class="arrow">&nbsp;</span><strong>Cobertura</strong> intensiva en social media durante el transcurso de la campaña</div>
+
+						<div><span class="arrow">&nbsp;</span><strong>Selección</strong>, notificación y entrega del premio al ganador</div>
+
+						<div class="odd"><span class="arrow">&nbsp;</span><strong>Mención</strong> destacada en el Newsletter sobre el concurso</div>
+
+						<div><span class="arrow">&nbsp;</span><strong>10%</strong> de dto para la proxima promoción</div>
+					</td>
+					<td class="red">
+					
+						<div><span class="arrow">&nbsp;</span><strong>30 días</strong> de duración</div>
+
+						<div class="odd"><span class="arrow">&nbsp;</span><strong>Customización Absoluta:</strong> Diseño banner para HomePage + interna (la del propio sorteo) + background personalizado </div>
+
+						<div><span class="arrow">&nbsp;</span><strong>Inclusion de un featured video</strong> en la pagina del concurso (opcional)</div>
+
+						<div class="odd"><span class="arrow">&nbsp;</span><strong>Acceso ilimitado a insights</strong> en tiempo real para monitoreo de campaña</div>
+
+						<div><span class="arrow">&nbsp;</span><strong>Reporte de performance</strong> pormenorizado al término de la campana</div>
+
+						<div class="odd"><span class="arrow">&nbsp;</span><strong>Almacenamiento</strong> de contactos para futura campaña</div>
+
+						<div><span class="arrow">&nbsp;</span><strong>Featured Post</strong> en la pag de FB de PST</div>
+
+						<div class="odd"><span class="arrow">&nbsp;</span><strong>Cobertura</strong> intensiva en social media durante el transcurso de la campaña</div>
+
+						<div><span class="arrow">&nbsp;</span><strong>Selección</strong>, notificación y entrega del premio al ganador</div>
+
+						<div class="odd"><span class="arrow">&nbsp;</span><strong>Mención</strong> destacada en el Newsletter sobre el concurso</div>
+
+						<div><span class="arrow">&nbsp;</span><strong>10%</strong> de dto para la proxima promoción</div>
+
+					</td>
+				</tr>	
+				<tr>
+					<td class="footer first green">$1500<br/><a href="#">COMPRAR</a></td>
+					<td class="footer orange">$2500<br/><a href="#">COMPRAR</a></td>
+					<td class="footer red">$3500<br/><a href="#">COMPRAR</a></td>
+				</tr>
+			</table>
+		</div>
 	</div>
 	</div>
 </div>	
